8700bb48c715e75e57d9b62421886914e223aee1
Kshitija Kanase <kshitijakanase@email.com>
2021-01-20 11:23:28 +0530

    Initial Commit

---
+++ Assignment_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_2
+//
+//  Created by kshitija kanase on 20/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
